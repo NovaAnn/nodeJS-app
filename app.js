4182f329c33d84afa27f21c1c2e4aba0e5796aaf
NovaAnn <novaannthomas@gmail.com>
2021-12-24 05:07:19 +0100

    commit

--- app.js
+++ app.js
@@ -1,89 +1,135 @@
 
+
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const { MongoClient } = require("mongodb");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 const multer = require("multer");
-const { graphqlHTTP } = require("express-graphql");
-const graphqlSchema = require("./controllers/graphql/schema");
-const graphqlResolver = require("./controllers/graphql/resolvers");
-const auth = require("./controllers/middleware/auth");
-const Stripe = require("stripe");
-const Order = require('./controllers/models/order');
 
-const app = express();
-const stripe = new Stripe('sk_test_51K5KCYJnr3G3bhPBBePnVK1NiUJPDZVez2ijSeGjv2BrPwHaKHMwmTPdkaVJtoNgXi7q8Vr6xvbOQdSQ78sMi19A00qBfuoRi0');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const port = process.env.PORT || 4000;
-app.use(bodyParser.json()); // application/json
+const MONGODB_URI =
+  "mongodb+srv://NovaAnn:kwmwBi9wmLJ0nYBw@cluster0.nn0w8.mongodb.net/myNodeDatabase?retryWrites=true&w=majority";
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log("Inside fileStorage");
+    cb(null, "images");
+  },
+
+  filename: (req, file, cb) => {
+    console.log("Inside filename");
+    cb(null, file.originalname);
+    console.log("After filename");
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  console.log("Inside filefilter");
+  console.log(file);
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    console.log("Inside filefilter-yes");
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin/add-product",
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
+
+  app.use("/admin/edit-product",
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
+
+  app.use("/admin/save-profile",
+  multer({ storage: fileStorage, fileFilter: fileFilter }).fields([{ name: 'profImage', maxCount: 1 }, { name: 'backImage', maxCount: 1 }]));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
+  console.log("Check 1");
+  console.log(req.session.isLoggedIn);
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
   next();
 });
 
-app.use(auth);
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err) {
-      if (!err.originalError) {
-        return err;
-      }
-      const data = err.originalError.data;
-      const message = err.message || "An error occurred.";
-      const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data };
-    },
-  })
-);
-
-app.use(
-  "/getstripesession/:amount",
-  async (req,res,next) => {
-    const amount = req.params.amount;
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency:"usd"
-    });
-    if (paymentIntent){
-      res.status(200).json(paymentIntent.client_secret);
-    } else {
-      res.status(500).json({statusCode:500,message:err.message});
-    }
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
   }
-);
+  console.log("Inside app.use");
+  console.log(req.session.user._id);
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+app.use("/admin", adminRoutes);
 
+app.use(shopRoutes);
+
+app.use(authRoutes);
+
+app.get("/500", errorController.get500);
+
+app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message: message, data: data });
+
+  res.status(500);
 });
 
-// app.listen(8000);
 mongoose
-  .connect(
-    "mongodb+srv://NovaAnn:kwmwBi9wmLJ0nYBw@cluster0.nn0w8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
-    app.listen(8000);
+    app.listen(port);
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
